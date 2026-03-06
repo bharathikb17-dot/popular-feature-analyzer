d623bd4f76c88a9635be49490a73f59a069f6535
bharathikb17-dot <bharathi.kb17@gmail.com>
2026-03-06 14:02:28 +0530

    Update popular_features.py

--- popular_features.py
+++ popular_features.py
@@ -1,1 +1,19 @@
+from collections import Counter
 
+def popularFeatures(features, requests, topN):
+
+    count = Counter()
+
+    for r in requests:
+        words = set(r.lower().split())
+
+        for f in features:
+            if f in words:
+                count[f] += 1
+
+    sorted_features = sorted(
+        count.items(),
+        key=lambda x: (-x[1], x[0])
+    )
+
+    return [feature for feature, _ in sorted_features[:topN]]
